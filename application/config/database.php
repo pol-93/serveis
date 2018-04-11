c83fae345b667f717cfed6b79853552ea41c6734
unknown <TAC_Pol_Cerdan@auria.local>
2018-04-11 10:47:54 +0200

    bd

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 //91.121.151.193
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '91.121.151.193',
+	'hostname' => 'localhost',
 	'username' => 'auriasoft',
 	'password' => 'G35t10$16',
 	'database' => 'AuriaSoft',
